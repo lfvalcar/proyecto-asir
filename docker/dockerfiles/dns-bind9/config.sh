8b2f57c576a61a33a545db5cb68f5720bc97742a
lfvalcar <luisfer998845@gmail.com>
2024-05-18 08:31:30 +0200

    Agregar variables entre el script de configuración y el dockerfile

--- docker/dockerfiles/dns-bind9/config.sh
+++ docker/dockerfiles/dns-bind9/config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Variables utilizadas
-# $ip - ip asignada al contenedor (en caso de tener varias utiliza la primera)
+# Variables utilizadas y obligatorias especificadas en el dockerfile o en el docker-compose
+# $ipv4_conatiner - ip asignada al contenedor
+# $ipv4_anfitrion - ip asignada al anfitrion
 # $dir_zonas - ruta donde se encuentran los archivos de zonas
 # $domain - nombre de dominio completo
 # $forwarders - servidores reenviadores para el servicio DNS
@@ -18,41 +19,49 @@
 # Cargar variables de entorno
 set -e
 
-# Creamos el directorio de las zonas
-mkdir $dir_zonas
+# Creamos el directorio de las zonas y sus archivos de zonas
+mkdir $dir_zonas && cp /etc/bind/db.empty ${dir_zonas}/db.${ipv4_anfitrion} && \
+      cp /etc/bind/db.empty ${dir_zonas}/db.${ipv4_container} &&  \
+      cp /etc/bind/db.empty ${dir_zonas}/db.${domain}
 
 # Establecer el servidor de nombres en /etc/resolv.conf
-# Establecemos la ip actual del contenedor, en el caso de que tenga varias se informa y se establece la primera
-if [ $(echo hostname -I | wc -w) > 1 ]; then
-  echo 'El contenedor tiene asignada más de una ip, se establecerá como servidor de nombres la primera'
-  ip=$(hostname -I | awk '{print $1}')
-  sed -i "s/nameserver 192.168.1.1/nameserver $ip" /etc/resolv.conf
+# Establecemos la ip actual del contenedor
+if [ $(hostname -I | grep -q "$ipv4_container" ) -eq 0 ]; then
+  echo 'La ipv4 asignada al contenedor no concuerda con la variable ipv4_container'
+  ipv4_container=$(hostname -I | awk '{print $1}')
+  echo "Se establecerá esta ipv4 $(hostname -I | awk '{print $1}') en el /etc/resolv.conf"
+  sed -i "s/nameserver 192.168.1.1/nameserver $ipv4_container" /etc/resolv.conf
 else
-  echo 'El contenedor tiene asignada una ip, se establecerá la misma como servidor de nombres'
-  ip=$(hostname -I)
-  sed -i "s/nameserver 192.168.1.1/nameserver $ip" /etc/resolv.conf
-fi # [ $(echo hostname -I | wc -w) > 1 ]
+  sed -i "s/nameserver 192.168.1.1/nameserver $ipv4_container" /etc/resolv.conf
+fi # [ $(hostname -I | grep -q "$ipv4_container" ) -eq 0 ]
 
 # Comprobar de que no existe rastro de las zonas en el archivo /etc/bind/named.conf.local
-if [ $(grep -cE "$domain|$ip" /etc/bind/named.conf.local) -eq 0 ]; then
+if [ $(grep -cE "$domain|$ipv4_container|$ipv4_anfitrion" /etc/bind/named.conf.local) -eq 0 ]; then
   echo 'Se establece la zona directa y inversa del servicio DNS en el archivo /etc/bind/named.conf.local'
-  # Establecer la zona directa de dominio en /etc/bind/named.conf.local
+  # Establecer la zona directa del dominio en /etc/bind/named.conf.local
   # zone "$domain" {
   #    type master;
   #    file "${dir_zonas}/db.${domain}";
   # };
   echo -e "//\nzone "$domain" {\n\ttype master;\n\tfile "${dir_zonas}/db.${domain}";\n};" >> /etc/bind/named.conf.local
 
-  # Establecer la zona inversa de dominio en /etc/bind/named.conf.local
-  # zone "$ip reverse" {
+  # Establecer la zona inversa (contenedor) del dominio en /etc/bind/named.conf.local
+  # zone "$ipv4_container reverse" {
   #    type master;
-  #    file "${dir_zonas}/db.${ip}";
+  #    file "${dir_zonas}/db.${ipv4_container}";
   # };
-  echo -e "//\nzone "$($ip | rev)" {\n\ttype master;\n\tfile "${dir_zonas}/db.${ip}";\n};" >> /etc/bind/named.conf.local
+  echo -e "//\nzone "$($ipv4_container | rev)" {\n\ttype master;\n\tfile "${dir_zonas}/db.${ipv4_container}";\n};" >> /etc/bind/named.conf.local
+
+  # Establecer la zona inversa (anfitrion) del dominio en /etc/bind/named.conf.local
+  # zone "$ipv4_anfitrion reverse" {
+  #    type master;
+  #    file "${dir_zonas}/db.${ipv4_anfitrion}";
+  # };
+  echo -e "//\nzone "$($ipv4_anfitrion | rev)" {\n\ttype master;\n\tfile "${dir_zonas}/db.${ipv4_anfitrion}";\n};" >> /etc/bind/named.conf.local
 
 else
   echo 'Ya existen las zonas en el archivo /etc/bind/named.conf.local'
-fi # [ $(grep -cE "$domain|$ip" /etc/bind/named.conf.local) -eq 0 ]
+fi # [ $(grep -cE "$domain|$ipv4_container|$ipv4_anfitrion" /etc/bind/named.conf.local) -eq 0 ]
 
 if [ $(grep -cE "^[^/]*forwarders" /etc/bind/named.conf.options) -eq 0 ]; then
   echo 'Se establece los reenviadores del servicio DNS en el archivo /etc/bind/named.conf.options'
