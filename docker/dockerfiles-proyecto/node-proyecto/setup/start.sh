07c7955ced2c68e381046f3bf33015ec96e71988
lfvalcar <luisfer998845@gmail.com>
2024-06-03 20:12:32 +0200

    mejora imagenes node

--- docker/dockerfiles-proyecto/node-proyecto/setup/start.sh
+++ docker/dockerfiles-proyecto/node-proyecto/setup/start.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 
-# Script estándar
+# Variables de entorno
+# ${HOME} - Variable que contiene el home de usuario actual
 
-# Cargar variables de entorno y funciones
+# Variables externas obligatorias provenientes del dockerfile o del docker-compose
+# ${id_rsa} - El archivo que contiene la clave rsa
+# $repo - Repositorio del proyecto que utiliza node y npm como gestor de paquetes
+
+# Comandos/utilidades utilizados
+# echo
+# cat
+# tail
+# service
+# npm
+# cd
+
+# Cargar variables de entorno
 set -e
-source /root/setup/libraries.sh
 
-# Configuración del servicio SSH + Acceso remoto usuario + inicio del servicio SSH
-config_ssh
+# Añadir la clave rsa al home del usuario administrador
+config_ssh(){
+    if [ -d ${HOME}/.ssh ]; then
+        cat /root/setup/${id_rsa} >> ${HOME}/.ssh/authorized_keys
+        echo 'ID RSA establecida en authorized_keys'
+    else
+        echo "El directorio $HOME/.ssh no existe, no se añade la clave rsa"
+    fi
 
-# Mantener contenedor en ejecución
-tail -f /dev/null
+    # Iniciar servicio SSH
+    service ssh start
+}
 
+npm_depends(){
+    # Actualización de npm y instalación de dependencias del repositorio del proyecto
+    npm update -g npm && cd $repo && npm i
+}
+
+main(){
+    # Configuración del servicio SSH + Acceso remoto usuario + inicio del servicio SSH
+    config_ssh
+
+    # Actualización y instalación de dependencias del repositorio
+    npm_depends
+
+    # Mantener contenedor en ejecución
+    tail -f /dev/null
+}
+
+main
